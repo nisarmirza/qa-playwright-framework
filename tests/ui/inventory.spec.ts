d6809f62891662cd4c1d4773839f315047c3d79f
Nisar Mirza <nisartaja@gmail.com>
2026-02-13 17:39:06 -0800

    refactor: use authedTest fixture in inventory spec

--- tests/ui/inventory.spec.ts
+++ tests/ui/inventory.spec.ts
@@ -1,22 +1,12 @@
-import { test } from "@playwright/test";
+import { test } from "../../fixtures/authedTest";
 import { LoginPage } from "../../pages/LoginPage";
-import { InventoryPage } from "../../pages/InventoryPage";
-import { test1 } from "../../fixtures/authedTest";
 
-
-test("@smoke add item updates cart badge", async ({ page }) => {
+test("@smoke add item updates cart badge", async ({ page, inventory }) => {
   const login = new LoginPage(page);
-  const inventory = new InventoryPage(page);
 
   await login.goto();
   await login.login("standard_user", "secret_sauce");
-  await inventory.expectOnInventory();
 
-  await inventory.addItem("Sauce Labs Backpack");
-  await inventory.expectCartCount(1);
-});
-
-test1("@smoke add item updates cart badge", async ({ page, inventory }) => {
   await page.goto("/inventory.html");
   await inventory.addItem("Sauce Labs Backpack");
   await inventory.expectCartCount(1);
