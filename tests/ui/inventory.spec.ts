91b98a2e480fcfef19da5dca0ee3f711377cb651
Nisar Mirza <nisartaja@gmail.com>
2026-02-13 17:41:51 -0800

    refactor: inventory spec uses storageState auth

--- tests/ui/inventory.spec.ts
+++ tests/ui/inventory.spec.ts
@@ -2,11 +2,6 @@ import { test } from "../../fixtures/authedTest";
 import { LoginPage } from "../../pages/LoginPage";
 
 test("@smoke add item updates cart badge", async ({ page, inventory }) => {
-  const login = new LoginPage(page);
-
-  await login.goto();
-  await login.login("standard_user", "secret_sauce");
-
   await page.goto("/inventory.html");
   await inventory.addItem("Sauce Labs Backpack");
   await inventory.expectCartCount(1);
